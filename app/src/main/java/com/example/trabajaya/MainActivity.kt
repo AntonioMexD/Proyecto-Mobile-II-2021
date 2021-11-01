46cd20e841ed0b78f96e3240ef17bc6028daa402
Javier Loayza Flores <javo_tralll@hotmail.com>
2021-11-01 09:53:56 -0400

    agregando barra de navegacion

--- app/src/main/java/com/example/trabajaya/MainActivity.kt
+++ app/src/main/java/com/example/trabajaya/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.trabajaya
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -8,7 +9,10 @@ import android.widget.Spinner
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil_usuario)
+        setContentView(R.layout.activity_pagina_inicio)
+
+        val intentxd = Intent(this,PaginaInicio::class.java)
+        startActivity(intentxd)
 
     }
 }
