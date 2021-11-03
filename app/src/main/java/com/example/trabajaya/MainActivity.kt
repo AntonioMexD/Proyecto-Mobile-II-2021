da7d5f3a89e926c42711fca5ceb63747212ca7ca
Antonio Medina <toomedinaamp2010@gmail.com>
2021-11-03 00:52:42 -0400

    Se trabajo en Login y register

--- app/src/main/java/com/example/trabajaya/MainActivity.kt
+++ app/src/main/java/com/example/trabajaya/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.trabajaya
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagina_inicio)
+<<<<<<< HEAD
 
         val intentxd = Intent(this,PaginaInicio::class.java)
         startActivity(intentxd)
 
+=======
+>>>>>>> 46ccd75 (Se trabajo en Login y register)
     }
 }
---
+++ app/src/main/java/com/example/trabajaya/OlvidarContrasena.kt
@@ -0,0 +1,54 @@
+package com.example.trabajaya
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
+
+class OlvidarContrasena : AppCompatActivity() {
+    private lateinit var txtEmail: EditText
+    private lateinit var auth:FirebaseAuth
+    private lateinit var progressBar: ProgressBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_olvidar_contrasena)
+        txtEmail=findViewById(R.id.txtEmail)
+        progressBar=findViewById(R.id.progressBar)
+        auth= FirebaseAuth.getInstance()
+
+        //actionbar
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Trabaja Ya!"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+
+    fun send(view: View){
+        val email=txtEmail.text.toString()
+
+        if (!TextUtils.isEmpty(email)){
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(this){
+                    task ->
+                    if (task.isSuccessful){
+                        progressBar.visibility=View.VISIBLE
+                        startActivity(Intent(this, IniciarSesion::class.java))
+                    }else{
+                        Toast.makeText(this, "Error al enviar el email", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+}
