3f3f4d7b76ae1302bc4f9959f97f66a1ebcecadd
Antonio Medina <toomedinaamp2010@gmail.com>
2021-11-03 10:32:33 -0400

    Listo para mergear

--- app/src/main/java/com/example/trabajaya/MainActivity.kt
+++ app/src/main/java/com/example/trabajaya/MainActivity.kt
@@ -8,12 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagina_inicio)
-<<<<<<< HEAD
 
         val intentxd = Intent(this,PaginaInicio::class.java)
         startActivity(intentxd)
 
-=======
->>>>>>> 46ccd75 (Se trabajo en Login y register)
     }
 }
